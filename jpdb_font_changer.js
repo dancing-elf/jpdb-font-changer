bfbf744517f1d7b87cbfdcdd776c715d8baa5dae
Anton Saraev <antonysaraev@gmail.com>
2023-05-21 22:57:26 +0300

    Update jpdb_font_changer.js

--- jpdb_font_changer.js
+++ jpdb_font_changer.js
@@ -12,29 +12,32 @@
 const defaultFonts = '"Nunito Sans","Extra Sans JP","Noto Sans Symbols2","Segoe UI",' +
     '"Noto Sans JP","Noto Sans CJK JP","Hiragino Sans GB","Meiryo",sans-serif';
 
-// you may want to use something like
+// you may add your favorite font in the begining of list
 // YuKyokasho
 // Tsukushi A Round Gothic
 // Klee
 // Kaiti SC
-const preferredFonts = 'Klee';
+// https://mhdigital.llc/web-typography-in-japanese/
+const preferredFonts = '"YuKyokasho", "ヒラギノ角ゴ ProN" , "Hiragino Kaku Gothic ProN" , "游ゴシック" , "游ゴシック体" , ' +
+    '"YuGothic" , "Yu Gothic" , "メイリオ" , "Meiryo" , "ＭＳ ゴシック" , "MS Gothic" , "HiraKakuProN-W3" , ' +
+    '"TakaoExゴシック" , "TakaoExGothic" , "MotoyaLCedar" , "Droid Sans Japanese" , "sans-serif"';
 const preferredCustomMeaningFonts = defaultFonts;
 
 (function() {
     'use strict';
+    setFont();
+    // needed for review page
+    waitForElm(`'.subsection-pitch-accent' .subsection`).then((elm) => {
+        setFont();
+    });
+})();
 
-    let body = document.querySelector('body');
-    body.style.fontFamily = preferredFonts;
-
-    for (let plain of document.querySelectorAll('.plain')) {
-        plain.style.fontFamily = preferredFonts;
-    }
-    for (let ruby of document.querySelectorAll('ruby')) {
-        ruby.style.fontFamily = preferredFonts;
-    }
-    for (let plain of document.querySelectorAll('.nav-item')) {
-        plain.style.fontFamily = preferredFonts;
-    }
+function setFont() {
+    setFontForAllElement('body');
+    setFontForAllElement('.plain');
+    setFontForAllElement('ruby');
+    setFontForAllElement('.nav-item');
+    setFontForAllElement('.description');
 
     // Cyrillic fonts are possible in custom meaning so don't apply custom font here
     // because many of them can't work with cyrillic fonts properly
@@ -42,4 +45,34 @@ const preferredCustomMeaningFonts = defaultFonts;
     if (customMeaning) {
         customMeaning.style.fontFamily = preferredCustomMeaningFonts;
     }
-})();
+}
+
+function setFontForAllElement(selector) {
+    for (let n of document.querySelectorAll(selector)) {
+        setFontForElement(n);
+    }
+}
+
+function setFontForElement(node, redefineColor) {
+    node.style.fontFamily = preferredFonts;
+}
+
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
